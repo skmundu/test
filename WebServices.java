5c19b581ea2bb480313460979c85d340ea9900aa
Sanjay <sanjay.mundu2000@gmail.com>
2018-10-05 22:30:10 +0530

    Master branceh commit

--- WebServices.java
+++ WebServices.java
@@ -3,7 +3,8 @@ public class WebServices
 
 public static void main(String[] args)
 {
-System.out.println("This is a test1");
+System.out.println("This is a test1 This change is made in Feature branch ");
+**** This change is done in master branch***
 }
 
 }
